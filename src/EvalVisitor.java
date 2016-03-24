fd18e8e9dcaff9214499cac31d8a9ed77ebf9d66
Jorge E. García <jores10@hotmail.com>
2016-03-24 10:50:18 -0600

    la gramatica de sql y su visitor
    incluye el EvalVisitor donde vamos a configurar lo que hace

---
+++ src/EvalVisitor.java
@@ -0,0 +1,12 @@
+
+public class EvalVisitor<T> extends SqlBaseVisitor<Object> {
+	
+	
+	//*** Todo visitor va de esta forma, podemos retornos cualquier cosa
+	  public T visitSql2003Parser (SqlParser.Sql2003ParserContext ctx) {
+		  
+		  return (T)"";
+	  }
+	
+	
+}
