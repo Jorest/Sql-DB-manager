cc1fc392ff898141ae25a024e6e35bcfc680bd17
Jorge E. García <jores10@hotmail.com>
2016-03-29 10:10:53 -0600

    Cambios visitor

---
+++ src/Dato.java
@@ -0,0 +1,35 @@
+
+public class Dato {
+	
+	private int integer ;
+	private char character ;
+	private float floating ;
+	private String date ;
+	public int getInteger() {
+		return integer;
+	}
+	public void setInteger(int integer) {
+		this.integer = integer;
+	}
+	public char getCharacter() {
+		return character;
+	}
+	public void setCharacter(char character) {
+		this.character = character;
+	}
+	public float getFloating() {
+		return floating;
+	}
+	public void setFloating(float floating) {
+		this.floating = floating;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+
+}
