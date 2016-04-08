33091bc774bde06adf15bf672e2c771db04708a9
RiverKev95 <riv13389@uvg.edu.gt>
2016-04-07 18:03:22 -0600

    Update Dato.java

--- src/Dato.java
+++ src/Dato.java
@@ -1,3 +1,12 @@
+/*
+ * Universidad del Valle Guatemala
+ * CC3040 Bases de datos
+ * Proyecto 1: DBMS
+ * Jorge Estuardo Garcia 13175
+ * Luis Humberto Duarte 13003
+ * Kevin Eduardo Rivera 13389
+ */
+
 import java.util.ArrayList;
 
 public class Dato {
